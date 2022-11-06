cf425a0814e0afbb3306de19804fa4847e08d711
Michele Cozzola <cozzolamichele89@gmail.com>
2022-11-06 19:55:51 +0100

    sistemato search

--- script.js
+++ script.js
@@ -1,102 +1,102 @@
 let tracks = [
     {   url : './audio/ACDC - Thunderstruck.mp3' ,
-    cover : './cover/cover acdc.jpg', artist : 'ACDC' , title : 'Thunderstruck'},
+    cover : './cover/cover acdc.jpg', artist : 'ACDC' , title : 'Thunderstruck', id : 1},  
     {   url : './audio/Green Day - Boulevard of Broken Dreams.mp3' ,
-    cover : './cover/cover american idiot.jpg', artist : 'GREEN DAY' , title : 'Boulevard Of Broken Dreams'},
+    cover : './cover/cover american idiot.jpg', artist : 'GREEN DAY' , title : 'Boulevard Of Broken Dreams', id: 2},
     {   url : './audio/Guns n  Roses - Sweet Child O Mine.mp3' ,
-    cover : './cover/cover apetite for descruction.jpg', artist : `GUNS N' ROSES` , title : `Sweet Child O' Mine`},
+    cover : './cover/cover apetite for descruction.jpg', artist : `GUNS N' ROSES` , title : `Sweet Child O' Mine`, id: 3},
     {   url : './audio/Maneskin - Amandoti.mp3' ,
-    cover : './cover/cover teatro d ira.jpg', artist : 'MANESKIN' , title : 'Amandoti' },
+    cover : './cover/cover teatro d ira.jpg', artist : 'MANESKIN' , title : 'Amandoti', id : 4},
     {   url : `./audio/Gloria Gaynor - I Will Survive.mp3` ,
-    cover : './cover/cover love tracks.jpg', artist : 'GLORIA GAYNOR' , title : `I Will Survive`},
+    cover : './cover/cover love tracks.jpg', artist : 'GLORIA GAYNOR' , title : `I Will Survive`, id : 5},
     {   url : './audio/ACDC - Shoot to Thrill.mp3' ,
-    cover : './cover/cover back in black.jpg', artist : 'ACDC' , title : 'Shoot to Thrill'},
+    cover : './cover/cover back in black.jpg', artist : 'ACDC' , title : 'Shoot to Thrill', id : 6},
     {   url : './audio/Green Day - Holiday.mp3' ,
-    cover : './cover/cover american idiot.jpg', artist : 'GREEN DAY' , title : 'Holiday'},
+    cover : './cover/cover american idiot.jpg', artist : 'GREEN DAY' , title : 'Holiday', id: 7},
     {   url : `./audio/Guns N Roses - Don't Damn Me.mp3` ,
-    cover : './cover/cover use your illusion.jpg', artist : `GUNS N'ROSES` , title : `Don't Damn Me`},
+    cover : './cover/cover use your illusion.jpg', artist : `GUNS N'ROSES` , title : `Don't Damn Me`, id: 8},
     {   url : './audio/Pink Floyd - Another Brick In The Wall.mp3' ,
-    cover : './cover/cover the wall.jpg', artist : 'PINK FLOYD' , title : 'Another Brick In The Wall'},
+    cover : './cover/cover the wall.jpg', artist : 'PINK FLOYD' , title : 'Another Brick In The Wall', id: 9},
     {   url : './audio/All American Rejects - Gives You Hell.mp3' ,
-    cover : './cover/cover When The World Comes Down.jpg', artist : 'ALL AMERICAN REJECTS' , title : 'Gives You Hell'},
+    cover : './cover/cover When The World Comes Down.jpg', artist : 'ALL AMERICAN REJECTS' , title : 'Gives You Hell', id: 10},
     {   url : './audio/Måneskin - Chosen.mp3' ,
-    cover : './cover/cover chosen.jpg', artist : 'MANESKIN' , title : 'Chosen'},
+    cover : './cover/cover chosen.jpg', artist : 'MANESKIN' , title : 'Chosen', id: 11},
     {   url : './audio/Placebo - Every Me And Every You.mp3' ,
-    cover : `./cover/cover without you i'm nothing.jpg`, artist : 'PLACEBO' , title : 'Every Me And Every You'},
+    cover : `./cover/cover without you i'm nothing.jpg`, artist : 'PLACEBO' , title : 'Every Me And Every You', id: 12},
     {   url : './audio/Måneskin - Supermodel.mp3',
-    cover : './cover/cover Supermodel.jpg', artist: 'MANESKIN',  title : 'Supermodel'},
+    cover : './cover/cover Supermodel.jpg', artist: 'MANESKIN',  title : 'Supermodel', id: 13},
     {   url : './audio/Queen - Fat Bottomed Girls.mp3',
-    cover : './cover/cover jazz.jpg', artist: 'QUEEN',  title : 'Fat Bottomed Girls'},
+    cover : './cover/cover jazz.jpg', artist: 'QUEEN',  title : 'Fat Bottomed Girls', id:14},
     {   url : './audio/Jeff Buckley - Grace.mp3' ,
-    cover : './cover/cover grace.jpg', artist : 'JEFF BUCKLEY' , title : 'Grace'},
+    cover : './cover/cover grace.jpg', artist : 'JEFF BUCKLEY' , title : 'Grace', id:15},
     {   url : './audio/Muse - Hysteria.mp3' ,
-    cover : './cover/cover absolution.jpg', artist : 'MUSE' , title : 'Hysteria'},
+    cover : './cover/cover absolution.jpg', artist : 'MUSE' , title : 'Hysteria', id:16},
     {   url : './audio/Guns N Roses- Dont Cry (Alt Version).mp3' ,
-    cover : './cover/cover use your illusion 2.jpg', artist : `GUNS N' ROSES` , title : `Don't Cry (Alt)`},
+    cover : './cover/cover use your illusion 2.jpg', artist : `GUNS N' ROSES` , title : `Don't Cry (Alt)`, id:17},
     {   url : './audio/Omnia - Free Ra Huri.mp3' ,
-    cover : './cover/cover Musick and Poëtree.jpg', artist : 'OMNIA' , title : 'Free Ra Huri'},
+    cover : './cover/cover Musick and Poëtree.jpg', artist : 'OMNIA' , title : 'Free Ra Huri', id: 18},
     {   url : './audio/Guns N Roses - Paradise City.mp3' ,
-    cover : './cover/cover apetite for descruction.jpg', artist : `GUNS N' ROSES` , title : 'Paradise City'},
+    cover : './cover/cover apetite for descruction.jpg', artist : `GUNS N' ROSES` , title : 'Paradise City', id:19},
     {   url : '/audio/Elton John - Rocket Man.mp3' ,
-    cover : './cover/Cover Honky Chateau.jpg', artist : 'ELTON JOHN' , title : 'Rocket Man'},
+    cover : './cover/Cover Honky Chateau.jpg', artist : 'ELTON JOHN' , title : 'Rocket Man', id:20},
     {   url : `./audio/Red Hot Chili Peppers - Shes Only 18.mp3` ,
-    cover : './cover/cover stadium arcadium.jpg', artist : 'RHCP' , title : `She's Only 18`},
+    cover : './cover/cover stadium arcadium.jpg', artist : 'RHCP' , title : `She's Only 18`, id:21},
     {   url : `./audio/Slash, Derek Sherinian, Billy Idol - In the Summertime!.mp3` ,
-    cover : './cover/cover in the summertime.jpg', artist : 'SLASH, BILLY IDOL' , title : `In the Summertime`},
+    cover : './cover/cover in the summertime.jpg', artist : 'SLASH, BILLY IDOL' , title : `In the Summertime`, id:22},
     {   url : './audio/Placebo - Running Up That Hill.mp3' ,
-    cover : './cover/cover Sleeping with Ghosts.jpg', artist : 'PLACEBO' , title : 'Running Up That Hill'},
+    cover : './cover/cover Sleeping with Ghosts.jpg', artist : 'PLACEBO' , title : 'Running Up That Hill', id:23},
     {   url : './audio/Maneskin - I Wanna Be Your Slave.mp3' ,
-    cover : './cover/cover teatro d ira.jpg', artist : 'MANESKIN' , title : `I Wanna Be Your Slave`},
+    cover : './cover/cover teatro d ira.jpg', artist : 'MANESKIN' , title : `I Wanna Be Your Slave`, id:24},
     {   url : `./audio/4 Non Blondes - What's Up .mp3` ,
-    cover : './cover/cover bigger.jpg', artist : '4 NON BLONDES' , title : `What's Up`},
+    cover : './cover/cover bigger.jpg', artist : '4 NON BLONDES' , title : `What's Up`, id:25},
     {   url : './audio/Guns n Roses - Dont Cry.mp3' ,
-    cover : './cover/cover use your illusion.jpg', artist : `GUNS N' ROSES` , title : `Don't Cry`},
+    cover : './cover/cover use your illusion.jpg', artist : `GUNS N' ROSES` , title : `Don't Cry`, id:26},
     {   url : './audio/Måneskin - Coraline.mp3' ,
-    cover : './cover/cover teatro d ira.jpg', artist : 'MANESKIN' , title : 'Coraline'},
+    cover : './cover/cover teatro d ira.jpg', artist : 'MANESKIN' , title : 'Coraline', id:27},
     {   url : './audio/Beatles - Let It Be.mp3' ,
-    cover : './cover/cover Let It Be.jpg', artist : 'BEATLES' , title : 'Let It Be'},
+    cover : './cover/cover Let It Be.jpg', artist : 'BEATLES' , title : 'Let It Be', id:28},
     {   url : './audio/David Bowie - Heroes Lyrics.mp3' ,
-    cover : './cover/cover heroes.jpg', artist : 'DAVID BOWIE' , title : 'Heroes'},
+    cover : './cover/cover heroes.jpg', artist : 'DAVID BOWIE' , title : 'Heroes', id:29},
     {   url : './audio/Green Day - Stray Heart.mp3' ,
-    cover : './cover/cover ¡dos!.jpg', artist : 'GREEN DAY' , title : 'Stray Heart'},
+    cover : './cover/cover ¡dos!.jpg', artist : 'GREEN DAY' , title : 'Stray Heart', id:30},
     {   url : './audio/Kate Bush - Running Up That Hill (A Deal With God).mp3' ,
-    cover : './cover/cover hunds of love.jpg', artist : 'KATE BUSH' , title : 'Running Up That Hill (A Deal With God)'},
+    cover : './cover/cover hunds of love.jpg', artist : 'KATE BUSH' , title : 'Running Up That Hill (A Deal With God)', id:31},
     {   url : 'audio/Pink Floyd - Comfortably Numb.mp3' ,
-    cover : './cover/cover the wall.jpg', artist : 'PINK FLOYD' , title : 'Comfortambly Numb'},
+    cover : './cover/cover the wall.jpg', artist : 'PINK FLOYD' , title : 'Comfortambly Numb', id:32},
     {   url : './audio/Red Hot Chili Peppers - Tell Me Baby.mp3' ,
-    cover : './cover/cover stadium arcadium.jpg', artist : 'RHCP' , title : 'Tell Me Baby'},
+    cover : './cover/cover stadium arcadium.jpg', artist : 'RHCP' , title : 'Tell Me Baby', id:33},
     {   url : './audio/Queen - Bohemian Rhapsody.mp3' ,
-    cover : './cover/cover a night at the opera.jpg', artist : 'QUEEN' , title : 'Bohemian Rhapsody'},
+    cover : './cover/cover a night at the opera.jpg', artist : 'QUEEN' , title : 'Bohemian Rhapsody', id:34},
     {   url : './audio/Green Day - Wake Me Up When September Ends.mp3' ,
-    cover : './cover/cover american idiot.jpg', artist : 'GREEN DAY' , title : 'Wake Me Up When September Ends'},
+    cover : './cover/cover american idiot.jpg', artist : 'GREEN DAY' , title : 'Wake Me Up When September Ends', id:35},
     {   url : './audio/Red Hot Chili Peppers - Dani California.mp3' ,
-    cover : './cover/cover stadium arcadium.jpg', artist : 'RHCP' , title : 'Dani California'},
+    cover : './cover/cover stadium arcadium.jpg', artist : 'RHCP' , title : 'Dani California', id:36},
     {   url : './audio/Guns N Roses - November Rain.mp3' ,
-    cover : './cover/cover use your illusion.jpg', artist : `GUNS N' ROSES` , title : 'November Rain'},
+    cover : './cover/cover use your illusion.jpg', artist : `GUNS N' ROSES` , title : 'November Rain', id:37},
     {   url : './audio/Green Day - 21 Guns.mp3' ,
-    cover : './cover/cover 21st Century Breakdown.jpg', artist : 'GREEN DAY' , title : '21 Guns'},
+    cover : './cover/cover 21st Century Breakdown.jpg', artist : 'GREEN DAY' , title : '21 Guns', id:38},
     {   url : `./audio/Queen - Don't Stop Me Now.mp3` ,
-    cover : './cover/cover jazz.jpg', artist : 'QUEEN' , title : `Don't Stop Me Now`},
+    cover : './cover/cover jazz.jpg', artist : 'QUEEN' , title : `Don't Stop Me Now`, id: 39},
     {   url : './audio/Red Hot Chili Peppers - Scar Tissue.mp3' ,
-    cover : './cover/cover californication.jpg', artist : 'RHCP' , title : 'Scar Tissue'},
+    cover : './cover/cover californication.jpg', artist : 'RHCP' , title : 'Scar Tissue', id:40},
     {   url : `./audio/Guns n Roses - Knockin' on Heaven's Door.mp3` ,
-    cover : './cover/cover use your illusion 2.jpg', artist : `GUNS N' ROSES` , title : `Knockin' on Heaven's Door`},
+    cover : './cover/cover use your illusion 2.jpg', artist : `GUNS N' ROSES` , title : `Knockin' on Heaven's Door`, id:41},
     {   url : './audio/Måneskin - Vent Anni.mp3' ,
-    cover : './cover/cover teatro d ira.jpg', artist : 'MANESKIN' , title : `Vent' Anni`},
+    cover : './cover/cover teatro d ira.jpg', artist : 'MANESKIN' , title : `Vent' Anni`, id:42},
     {   url : './audio/Red Hot Chili Peppers - Stadium Arcadium.mp3' ,
-    cover : './cover/cover stadium arcadium.jpg', artist : 'RHCP' , title : 'Stadium Arcadium'},
+    cover : './cover/cover stadium arcadium.jpg', artist : 'RHCP' , title : 'Stadium Arcadium', id:43},
     {   url : 'audio/Pink Floyd - Wish You Were Here.mp3',
-    cover : './cover/cover whish you where here.jpg', artist : 'PINK FLOYD' , title : 'Whish You Where Here'},
+    cover : './cover/cover whish you where here.jpg', artist : 'PINK FLOYD' , title : 'Whish You Where Here', id:44},
     {   url : './audio/Maneskin - Torna A Casa.mp3' ,
-    cover : './cover/cover il ballo della vita.jpg', artist : 'MANESKIN' , title : `Torna A Casa`},
+    cover : './cover/cover il ballo della vita.jpg', artist : 'MANESKIN' , title : `Torna A Casa`, id:45},
     {   url : './audio/Queen - Radio Ga Ga.mp3' ,
-    cover : './cover/cover the works.jpg', artist : 'QUEEN' , title : 'Radio Ga Ga'},
+    cover : './cover/cover the works.jpg', artist : 'QUEEN' , title : 'Radio Ga Ga', id:46},
     {   url : './audio/Green Day - American Idiot.mp3' ,
-    cover : './cover/cover american idiot.jpg', artist : 'GREEN DAY' , title : 'American Idiot'},    
-    {   url : './audio/Nirvana - Smells Like Teen Spirit.mp3' ,
-    cover : './cover/cover nevermind.jpg', artist : 'NIRVANA' , title : ' Smells Like Teen Spirit'},    
+    cover : './cover/cover american idiot.jpg', artist : 'GREEN DAY' , title : 'American Idiot', id:47},    
+    {   url : '/audio/Nirvana - Smells Like Teen Spirit.mp3' ,
+    cover : './cover/cover nevermind.jpg', artist : 'NIRVANA' , title : ' Smells Like Teen Spirit', id:48},    
     {   url : './audio/Maneskin - Zitti E Buoni.mp3' ,
-    cover : './cover/cover teatro d ira.jpg', artist : 'MANESKIN' , title : 'Zitti E Buoni'},
+    cover : './cover/cover teatro d ira.jpg', artist : 'MANESKIN' , title : 'Zitti E Buoni', id:49},
 ]
 
 /* btns */
@@ -106,9 +106,12 @@ const pauseBtn = document.querySelector('#pause-btn');
 const rightBtn = document.querySelector('#right-btn');
 const leftBtn = document.querySelector('#left-btn');
 const shuffleBtn = document.querySelector('#shuffle-btn');
+const searchBtn = document.querySelector('.fa-napster');
+const searchInput = document.querySelector('#search-input');
 
 /* track */
 const playlist = document.querySelector('.playlist');
+const search = document.querySelector('.search');
 const circleCd = document.querySelector('.circle-cd');
 let track = document.querySelector('#track');
 const trackArtist = document.querySelector('#track-artist')
@@ -141,9 +144,10 @@ let grabbedVol = false;
 
 /* FUNCTIONS */
 
-function populateTrackList() {
-    tracks.forEach((track, index)=>{
+function populateTrackList(tracks,side) {
+    tracks.forEach((track)=>{
         let card = document.createElement('div');
+        card.innerHTML = '';
 
         card.classList.add('track-playlist');
         card.innerHTML = 
@@ -157,32 +161,33 @@ function populateTrackList() {
                 <span class="tc-accent font-cursive">${track.title}</span>
             </div>
             
-                <i data-track="${index}" id="playlist-play" class="fa-solid fa-circle-play tc-blue playlist-id fa-3x"></i>
+                <i data-track="${track.id -1}" id="playlist-play" class="fa-solid fa-circle-play tc-blue playlist-id fa-3x"></i>
                 <i id="playlist-pause" class="fa-solid fa-circle-pause tc-blue fa-3x "></i>
             
             </div
         `
-        playlist.appendChild(card);
+        side.appendChild(card);
     });
 
-    let playBtns = document.querySelectorAll('.playlist-id')
-    .forEach(btn=>btn.addEventListener('click', ()=>{
+    let playBtns = document.querySelectorAll('.playlist-id');
+    playBtns.forEach(btn=>btn.addEventListener('click', ()=>{
         let selectedTrack = btn.getAttribute('data-track');
         currentTrack = selectedTrack;
-        changeTrackDetails()
-        changePlaylistActive()
+        changeTrackDetails();
+        changePlaylistActive();
+        sidebarsRemove();
         if(playing ){
             play()
         }
         if (!playing) {
             play()
-        }
+        }        
     }))
 }
 
 function changePlaylistActive(){
-    let trackListCards = document.querySelectorAll('.track-card')
-    .forEach((card, index)=>{
+    let trackListCards = document.querySelectorAll('.track-card');
+    trackListCards.forEach((card, index)=>{
         if(index == currentTrack){
             card.classList.add('active');
         } else {
@@ -196,6 +201,9 @@ function playlistToggle() {
     playlist.classList.toggle('playlist-toggle')
 }
 
+function searchToggle() {
+    search.classList.toggle('search-toggle');
+}
 
 function play() {
     if (!playing) {
@@ -324,8 +332,10 @@ function buttonsEvents(ev) {
     }
 }
 
-function sidebarPlay() {
-    playlistToggle()       
+function sidebarsRemove() {
+    playlist.classList.remove('playlist-toggle');
+    search.classList.remove('search-toggle');
+    searchInput.value = '';
 }
 
 function updateVolumeCursor() {
@@ -405,6 +415,7 @@ function dragVolHandle(e){
 playBtn.addEventListener('click', play);
 pauseBtn.addEventListener('click', play);
 plailystBtn.addEventListener('click', playlistToggle);
+searchBtn.addEventListener('click', searchToggle)
 rightBtn.addEventListener('click', next);
 leftBtn.addEventListener('click', prev);
 track.addEventListener('ended', next)
@@ -417,7 +428,7 @@ volumeBtn.addEventListener('wheel', volumeWheel);
 
 
 /* CALL FUNCTIONS */
-populateTrackList()
+populateTrackList(tracks,playlist)
 changePlaylistActive()
 changeTrackDetails()
 updateVolumeCursor()
@@ -425,11 +436,17 @@ updateVolumeCursor()
 
 
 /* select track from sidebar */
-const playlistPlay = document.querySelectorAll('#playlist-play');
 
+searchInput.addEventListener('input' , (e)=>{
 
+    if (e.target.value.length > 3) {
+        let searched = e.target.value;
+        
+        let artist = tracks.filter(track=>track.artist.toLowerCase().includes(searched.toLowerCase()))
 
-playlistPlay.forEach((btn)=>{
-    btn.addEventListener('click', sidebarPlay)
-    
+        let title = tracks.filter(track=>track.title.toLowerCase().includes(searched.toLowerCase()));
+
+        populateTrackList(title,search);
+        populateTrackList(artist,search);
+    } 
 })
